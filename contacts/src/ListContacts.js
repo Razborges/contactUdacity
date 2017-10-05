b330e05319a169c3ec2973318c3b3fdfb3c76e83
razborges <razborges@me.com>
2017-10-04 23:06:13 -0300

    Aula React Router
    React Router e outras bibliotecas para o form de adição

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import escapeRegExp from 'escape-string-regexp';
 import sortBy from 'sort-by';
@@ -52,6 +53,10 @@ class ListContacts extends Component {
             onChange={event => this.updateQuery(event.target.value)}
           />
         {/*JSON.stringify(this.state)*/}
+          <Link
+            to='/create'
+            className='add-contact'
+          >Add Contact</Link>
         </div>
 
         {showingContacts.length !== contacts.length && (
